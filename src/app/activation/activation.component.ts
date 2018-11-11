4848f4a8f511f78ee82f57fcb4242b89a57ab3e7
online6731 <mohammad.parsian@gmail.com>
2018-11-11 20:15:58 +0330

    .

--- src/app/activation/activation.component.ts
+++ src/app/activation/activation.component.ts
@@ -1,39 +1,30 @@
-import { Component, OnInit, Injectable } 	from '@angular/core';
-import { HttpClient } 						from '@angular/common/http';
-import { RouterModule, Router } 			from '@angular/router';
-
-import { activationResponse }				from '../activationResponse';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { activationResponse } from '../activationResponse';
 
 @Component({
-  selector: 'app-activation',
-  templateUrl: './activation.component.html',
-  styleUrls: ['./activation.component.css']
+    selector: 'app-activation',
+    templateUrl: './activation.component.html',
+    styleUrls: ['./activation.component.css']
 })
 
 @Injectable()
 export class ActivationComponent implements OnInit {
 
-  constructor(
-  	private http	: HttpClient,
-  	public 	router 	: Router
-  ) { }
+    constructor(private http: HttpClient, public router: Router) { }
 
-  ngOnInit() {
-  }
-  
-  active(code): void {
-  	this.http.post<activationResponse>(`${localStorage.getItem("server")}/signup/activation/user/${localStorage.getItem("username")}/code/${code}`, { })
-		.subscribe(data => {
-			console.log(data);
-	
-      if (data.ok){
-        this.router.navigate(['/interests']);
-      }
-      else {
-      	
-      }
-      
-	});
-  }
+    ngOnInit() { }
 
+    active(code): void {
+        this.http.post<activationResponse>(
+            `${localStorage.getItem('server')}/signup/activation/user/${localStorage.getItem('username')}/code/${code}`, {})
+            .subscribe(data => {
+                if (data.ok) {
+                    this.router.navigate(['/interests']);
+                } else {
+
+                }
+        });
+    }
 }
