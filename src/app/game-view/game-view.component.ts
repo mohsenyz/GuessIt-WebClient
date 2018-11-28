4679c44d55fdd202126184fbd2fcc311a6fc425c
online6731 <mohammad.parsian@gmail.com>
2018-11-28 11:36:23 +0330

    .

--- src/app/game-view/game-view.component.ts
+++ src/app/game-view/game-view.component.ts
@@ -1,38 +1,34 @@
 import { Component,
-		 OnInit,
-		 Injectable }  
-	from '@angular/core';
+         OnInit,
+         Injectable }  
+    from '@angular/core';
 
 import { HttpClient }
-	from '@angular/common/http';
+    from '@angular/common/http';
 
 import { RouterModule,
-		 Router,
-		 ActivatedRoute
-		}
-	from '@angular/router';
+         Router,
+         ActivatedRoute
+        }
+    from '@angular/router';
 
 import { User }
-	from '../user';
+    from '../user';
 import { sendAnswerResponse }
-	from '../sendAnswerResponse';
+    from '../sendAnswerResponse';
 import { viewGameResponse }
-	from '../viewGameResponse';
-import { joinGameResponse }
-	from '../joinGameResponse';
+    from '../viewGameResponse';
+    import { joinGameResponse }
+    from '../joinGameResponse';
 
-
-import { MatGridListModule,
-		 MatButtonToggleModule,
-		 MatButtonModule,
-		 MatTabsModule,
-		 MatListModule,
-		}
-	from '@angular/material';
+import { Game }
+    from '../Game';
 
 import { GameService } 
-	from '../game.service';
+    from '../game.service';
 
+import { formatDate } from '@angular/common';
+    
 
 @Component({
   selector: 'app-game-view',
@@ -43,56 +39,57 @@ import { GameService }
 
 @Injectable()
 export class GameViewComponent implements OnInit {
-	
-	game = {};
-	isInGame = false;
-	teamNames = [];
-	myTeam = localStorage.getItem("username");
-
+    
+    game: Game;
+    isInGame = false;
+    teamNames = [];
+    myTeam = localStorage.getItem("username");
+    date;
 
   constructor(
-		private http    	: HttpClient,
-		public  router  	: Router,
-		private route 		: ActivatedRoute,
-    	private GameService : GameService
+        private http    	: HttpClient,
+        public  router  	: Router,
+        private route 		: ActivatedRoute,
+        private GameService : GameService
   ) { }
 
 
   ngOnInit() {
-  	this.viewGame(this.route.snapshot.params.gameID);
-  	localStorage.setItem("isInGame", "false");
+      this.viewGame(this.route.snapshot.params.gameID);
+      localStorage.setItem("isInGame", "false");
   }
 
 
   viewGame(gameID: string): void{
-  	this.GameService.viewGame(gameID).subscribe(
-		(viewGameResponse: viewGameResponse) => {
-			if (viewGameResponse.ok){
-				
-				this.game = viewGameResponse.game;
-    		    		
-	    		viewGameResponse.game.teams.forEach(function(team){
-	    			
-    				if (team.team == localStorage.getItem("username")){
-    					localStorage.setItem("isInGame", "true");
-    					//this.isInGame = true;
-    					console.log('you are in game');
-    				}
-	    			
-	    			team.members.forEach(function(player){
-	    				
-	    			});
-	    		});
+      this.GameService.viewGame(gameID).subscribe(
+        (viewGameResponse: viewGameResponse) => {
+            if (viewGameResponse.ok){
+                
+                this.game = viewGameResponse.game;
+                this.date = formatDate(this.game.rules.start.date, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0430');
 
-	    		viewGameResponse.game.teams.forEach(function(team){
-					this.teamNames.push(team.team);
-				});
+                viewGameResponse.game.teams.forEach(function(team){
+                    
+                    if (team.team == localStorage.getItem("username")){
+                        localStorage.setItem("isInGame", "true");
+                        //this.isInGame = true;
+                        console.log('you are in game');
+                    }
+                    
+                    team.members.forEach(function(player){
+                        
+                    });
+                });
 
-			}
-			else {
+                viewGameResponse.game.teams.forEach(function(team){
+                    this.teamNames.push(team.team);
+                });
 
-			}
-		}
+            }
+            else {
+
+            }
+        }
     );
   }
 
@@ -100,17 +97,17 @@ export class GameViewComponent implements OnInit {
 
   joinGame(gameID: string): void{
     this.GameService.joinGame(gameID, localStorage.getItem("username")).subscribe(
-		(joinGameResponse: joinGameResponse) => {
-			if (joinGameResponse.ok){
-				
-				this.viewGame(this.route.snapshot.params.gameID);
-				
-				//this.router.navigate([`/game/${gameID}/team/${localStorage.getItem("username")}/play`]);
-			}
-			else {
+        (joinGameResponse: joinGameResponse) => {
+            if (joinGameResponse.ok){
+                
+                this.viewGame(this.route.snapshot.params.gameID);
+                
+                //this.router.navigate([`/game/${gameID}/team/${localStorage.getItem("username")}/play`]);
+            }
+            else {
 
-			}
-		}
+            }
+        }
     );
   }
 
@@ -119,7 +116,7 @@ export class GameViewComponent implements OnInit {
   }
 
   inGame(){
-  	return localStorage.getItem("isInGame") == "true";
+      return localStorage.getItem("isInGame") == "true";
   }
 
 
--- src/app/game.ts
+++ src/app/game.ts
@@ -1,69 +1,69 @@
 import { Question }
-	from './question';
+    from './question';
 
 
 export class Game {
-	name					: string;
-	teams					: [{
-		team				: string;
-		
-		members			: [{
-			member		: string;
-			rounds		: [ number ];
-		}],
-		
-		rounds			: [{
-			members		: [{
-				member	: string;
-				answer	: {
-					content	: String;
-				}
-			}];
-			question	: Question;
-			score		: number;
-		}]
-	}];
-	
-	rules					: {
-		rounds			: [{
-			category	: string;
-			tags			: [ string ];
-			level			: {
-				min			: number;
-				max			: number;
-			}
-		}],
-		
-		teams				: {
-			level			: {
-				min			: number;
-				max			: number;
-			}
-		},
-		
-		start				: {
-			date			: Date;
-			teams			: number;
-		},
-		
-		duration		: number;
-		
-	};
-	
-	questions			: [Question];
-	
-	result				: {
-		winner			: string;
-	};
-	
-	statics				: {
-		teams				: number;
-	};
-	
-	creator				: string;
-	
-	started				: boolean;
+    name					: string;
+    teams					: [{
+        team				: string;
+        
+        members			: [{
+            member		: string;
+            rounds		: [ number ];
+        }],
+        
+        rounds			: [{
+            members		: [{
+                member	: string;
+                answer	: {
+                    content	: String;
+                }
+            }];
+            question	: Question;
+            score		: number;
+        }]
+    }];
+    
+    rules					: {
+        rounds			: [{
+            category	: string;
+            tags			: [ string ];
+            level			: {
+                min			: number;
+                max			: number;
+            }
+        }],
+        
+        teams				: {
+            level			: {
+                min			: number;
+                max			: number;
+            }
+        },
+        
+        start				: {
+            date			: Date;
+            teams			: number;
+        },
+        
+        duration		: number;
+        
+    };
+    
+    questions			: [Question];
+    
+    result				: {
+        winner			: string;
+    };
+    
+    statics				: {
+        teams				: number;
+    };
+    
+    creator				: string;
+    
+    started				: boolean;
 
-	timeToStart 		: number;
-	
+    timeToStart 		: number;
+    
 }
